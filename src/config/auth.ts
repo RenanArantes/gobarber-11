3399259db4d90b4d0d847a5f07c6d0715124684d
RenanArantes <renan.arantes@rede.ulbra.br>
2020-04-18 13:03:40 -0300

    Rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4008d78e581b34ac0d87682f650d4f02',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticationUserService.ts
+++ src/services/AuthenticationUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -32,9 +33,11 @@ class AuthenticationUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '4008d78e581b34ac0d87682f650d4f02', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     delete user.password;
