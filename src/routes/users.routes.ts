ed1399129cd98057f683753d8009526c4ad7735c
RenanArantes <renan.arantes@rede.ulbra.br>
2020-04-18 10:26:11 -0300

    criacao de registro e criptografia de senhas

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
+export default usersRouter;
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+/**
+ * Se nao formos ter metodos personalizados de relacionados ao banco
+ * nao e necessario criar um arquivo de repositories
+ */
+
+class CreateUserService {
+  async execute({ name, email, password }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUserExists = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await userRepository.save(user);
+
+    delete user.password;
+
+    return user;
+  }
+}
+export default CreateUserService;
