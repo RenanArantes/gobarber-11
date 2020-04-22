f3373ee493bdad5fe9ec36e545d9a51887e1c1a5
RenanArantes <renan.arantes@rede.ulbra.br>
2020-04-22 20:38:04 -0300

    criando tooltip de erros

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import getValidationErros from '../../utils/getValidationErrors';
 
 import logo from '../../assets/logo.svg';
 
@@ -11,8 +13,12 @@ import Input from '../../components/Input';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatorio'),
         email: Yup.string()
@@ -26,6 +32,10 @@ const SignUp: React.FC = () => {
       });
     } catch (err) {
       console.log(err);
+
+      const errors = getValidationErros(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -35,7 +45,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faca seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
