17828d6d412b1aaba82a37bb08811b8592b47e9a
RenanArantes <renan.arantes@rede.ulbra.br>
2020-04-30 21:27:40 -0300

    Importando fontes externas - 1 Estrutura e padroes

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
