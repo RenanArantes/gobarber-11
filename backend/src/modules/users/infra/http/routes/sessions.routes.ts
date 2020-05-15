5132126cd7ce3ccee879c933e6bf50ec9d4499bf
RenanArantes <renan.arantes@rede.ulbra.br>
2020-05-14 22:41:35 -0300

    Configurando os Imports - 1 Arquitetura DDD
    paths do node tsconfig cria atalho para as importacoes

--- backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import AuthenticationUserService from '../services/AuthenticationUserService';
+import AuthenticationUserService from '@modules/users/services/AuthenticationUserService';
 
 const sessionsRouter = Router();
 
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import routes from '@shared/infra/http/routes';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
