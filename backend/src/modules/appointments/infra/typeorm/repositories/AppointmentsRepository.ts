37fd6b30ec5d27c2067c402326605b9acac9d9dd
RenanArantes <renan.arantes@rede.ulbra.br>
2020-05-14 23:05:15 -0300

    Liskov Substitution Principle
    LSK e o L do SOLID, e diz que as camadas que sao integracoes com outras libs(como o db), devem oider ser substituidas(com a interface do TS nesse caso).

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,15 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
+class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository {
   // video: Repositorio TypeORM 4:50
-  public async findByDate(date: Date): Promise<Appointment | null> {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date },
     });
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
---
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,6 @@
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
+
+export default interface IAppointmentsRepository {
+  findByDate(date: Date): Promise<Appointment | undefined>;
+
+}
