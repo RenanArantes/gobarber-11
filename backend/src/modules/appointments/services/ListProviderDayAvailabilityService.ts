8e35122ee6b51c2ef2be90afac876395bce4792c
RenanArantes <renan.arantes@rede.ulbra.br>
2020-06-02 22:15:03 -0300

    Excluindo horarios antigos - 3 Agendamento - Nivel 04

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -44,12 +44,19 @@ class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
-      return { hour, available: !hasAppointmentInHour };
+      const compareDate = new Date(year, month - 1, day, hour);
+
+      return {
+        hour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
+      };
     });
 
     return availability;
