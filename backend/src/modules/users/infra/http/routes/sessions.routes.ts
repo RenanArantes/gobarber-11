230c5cdcd69dc5f367c0bc4901cf2e398856bef7
RenanArantes <renan.arantes@rede.ulbra.br>
2020-06-11 18:59:02 -0300

    Validando dados - 2 Personalizando para producao - Nivel 04

--- backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import SessionsController from '@modules/users/infra/http/controllers/SessionsController';
 
 const sessionsRouter = Router();
 const sessionsControler = new SessionsController();
 
-sessionsRouter.post('/', sessionsControler.create);
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsControler.create,
+);
 
 export default sessionsRouter;
