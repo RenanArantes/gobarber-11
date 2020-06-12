dd394d1ce86455b0dd83d0d015ece788567d64de
RenanArantes <renan.arantes@rede.ulbra.br>
2020-06-11 23:31:25 -0300

    Emails pelo Amazon SES - 2 Personalizando para producao - Nivel 04

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'email@configurado.com.br',
+      name: 'Dono do email',
+    },
+  },
+} as IMailConfig;
