28d5724dd4479961df4c7cf5457d31a95b467a18
RenanArantes <renan.arantes@rede.ulbra.br>
2020-06-13 10:43:24 -0300

    Configurando cache - 3 Cache e seguranca - Nivel 04
    Cache possibilita armazenar, por exemplo, queries emum lugar muito mais performatico usando o banco Redis
    
    O Redis nao tem tabelas, trabalha com chave e valor
    
    comando docker para o redis - docker run --name redis -p 6379:6379 -d -t redis:alphine
    
    ioredis e um drive de conexao do Redis com o js/ts
    
    set e a funcao de save do Redis

---
+++ backend/src/config/cache.ts
@@ -0,0 +1,21 @@
+import { RedisOptions } from 'ioredis';
+
+interface ICacheConfig {
+  driver: string;
+
+  config: {
+    redis: RedisOptions;
+  };
+}
+
+export default {
+  driver: process.env.CACHE_DRIVER,
+
+  config: {
+    redis: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
+    },
+  },
+} as ICacheConfig;
