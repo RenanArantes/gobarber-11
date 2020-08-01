fbe81204b272958a66f9b38d8eb9ca14e1eb0aba
RenanArantes <renan.arantes@rede.ulbra.br>
2020-07-31 21:39:54 -0300

    Teste de login - 2 Testando autenticacao - Testes no React - Nivel 05

--- frontend/src/__tests__/pages/SignIn.spec.tsx
+++ frontend/src/__tests__/pages/SignIn.spec.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
-import { render } from '@testing-library/react';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
-    Link: ({ children }: { children: React.ReactNode }) => children
-  }
-})
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
 
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />)
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
-  })
-})
+    const emailField = getByPlaceholderText('E-mail');
+    const paswordField = getByPlaceholderText('Password');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'email@email.com' } });
+    fireEvent.change(paswordField, { target: { value: '123456789' } });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+  });
+});
